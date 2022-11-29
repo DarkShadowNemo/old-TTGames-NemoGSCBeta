784ce3e19279b879fa23fe4b559a3d7c78a14244
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2022-11-29 11:48:48 -0800

    Add files via upload
    ...

---
+++ io_chunkscene_gsc/__init__.py
@@ -0,0 +1,88 @@
+bl_info = {
+        'name'			: 'Finding Nemo GSC Level Chunk Importer',
+	'author'		: 'DarkShadow Nemo',
+	'version'		: (0, 1, 5),
+	'blender'		: (3, 0, 0),
+	'location'		: 'File > Import',
+	'description'           : 'Import GSC one mesh chunk makes it easier',
+	'category'		: 'Chunk-Importer',
+}
+import os
+import bpy
+import importlib
+from bpy.props import CollectionProperty, StringProperty, BoolProperty, EnumProperty, FloatProperty, IntProperty
+from bpy_extras.io_utils import ImportHelper
+
+from.import gsc_chunk_importer
+
+class ImportChunkGSC(bpy.types.Operator, ImportHelper):
+        bl_idname  = 'import_chunk.gsc'
+        bl_label   = 'Import Chunk GSC'
+        bl_options = {'UNDO'}
+        filename_ext = '.gsc'
+        files: CollectionProperty(
+                name	    = 'File path',
+                description = 'File path used for finding the GSC Chunk file.',
+                type	    = bpy.types.OperatorFileListElement
+        )
+        WholeOne: BoolProperty(
+                name        = '0x03010001 all offsets',
+                description = 'if they match it imports all the whole chunk in'
+        )
+        ONE_CHUNK_OFFSET1: BoolProperty(
+                name        = '0x030100010380',
+                description = 'imports with vertices and triangle strips aligned up'
+        )
+        ONE_CHUNK_OFFSET2: BoolProperty(
+                name        = '0x000000000280',
+                description = 'imports with vertices but no triangle strips you have to do this manually'
+        )
+        SST: BoolProperty(
+                name        = 'SplineSet',
+                description = 'imports splines with vertices and edges'
+        )
+        SPEC: BoolProperty(
+                name        = 'SpecialSet',
+                description = 'imports SpecialSet Objects'
+	)
+        INST: BoolProperty(
+                name        = 'INSTANCE',
+                description = 'imports INSTANCE Objects with namedtable strings'
+        )
+        IABL: BoolProperty(
+                name        = 'InstanceAnimationBlock',
+                description = 'imports the stuff that you replaced in ram'
+        )
+        ONE_CHUNK_OFFSET1PT2: BoolProperty(
+                name = "0x030100010380 Part 2",
+                description = 'supports now dodgy lego star wars and nemo there nemo offset starts at 0x0480XX6D and uv and rgba data for lego star wars'
+        )
+        SelectOnlyUVMesh: BoolProperty(
+                name = "0x010000050480",
+                description = "be careful to select the mesh before executing or you will get the wrong uv data, select mesh first before doing this or it won't work"
+        )
+        SelectOnly2ndUVMesh: BoolProperty(
+                name = "0x010000050480",
+                description = "be careful to select the mesh before executing always on 0x65 next to it in a row"
+        )
+        SelectOnly3rdUVMesh: BoolProperty(
+                name = "0x0480XX65",
+                description = "be careful to select the mesh before executing, very short but done a workaround it this touches on this offset"
+        )
+        directory: StringProperty()
+        filter_glob: StringProperty(default = '*.gsc', options = {'HIDDEN'})
+        def execute(self, context):
+                paths = [os.path.join(self.directory, name.name) for name in self.files]
+                if not paths: paths.append(self.filepath)
+                importlib.reload(gsc_chunk_importer)
+                for path in paths: gsc_chunk_importer.parse_gsc(path, WholeOne = self.WholeOne, ONE_CHUNK_OFFSET1 = self.ONE_CHUNK_OFFSET1,ONE_CHUNK_OFFSET1PT2 = self.ONE_CHUNK_OFFSET1PT2,ONE_CHUNK_OFFSET2 = self.ONE_CHUNK_OFFSET2,SST = self.SST,SPEC = self.SPEC,INST = self.INST,IABL = self.IABL, SelectOnlyUVMesh = self.SelectOnlyUVMesh, SelectOnly2ndUVMesh = self.SelectOnly2ndUVMesh, SelectOnly3rdUVMesh = self.SelectOnly3rdUVMesh)
+                return {'FINISHED'}
+def menu_func_import(self, context):
+        self.layout.operator(ImportChunkGSC.bl_idname, text='GSC Chunk Importer (.gsc)')
+def register():
+        bpy.utils.register_class(ImportChunkGSC)
+        bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+def unregister():
+        bpy.utils.unregister_class(ImportChunkGSC)
+        bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+if __name__ == '__main__': register()
---
+++ io_chunkscene_gsc/gsc_chunk_importer.py
@@ -0,0 +1,427 @@
+from struct import pack, unpack
+import os
+import bpy
+
+chunk = 0 # what int property does not work with bool property might have to do this manually
+
+def wholeChunk1(f, vertices=[]):
+    f.seek(0)
+    while 1:
+        Chunk = f.read(4)
+        if Chunk == b"\x03\x01\x00\x01":
+            f.seek(2,1)
+            vertexCount = unpack("b", f.read(1))[0]
+            clump = unpack("b", f.read(1))[0]
+            for i in range(vertexCount):
+                vx = unpack("<f", f.read(4))[0]
+                vy = unpack("<f", f.read(4))[0]
+                vz = unpack("<f", f.read(4))[0]
+                nz = unpack("<f", f.read(4))[0] #normals
+                vertices.append([vx,vy,vz])
+            mesh = bpy.data.meshes.new("dragonjan")
+            mesh.from_pydata(vertices, [], [])
+            object = bpy.data.objects.new("dragonjan", mesh)
+            bpy.context.collection.objects.link(object)
+        elif Chunk == b"SST0":
+            print(f.tell())
+            break
+
+"""def get_1stobjectspt3(f, vertices=[], faces=[], fa=-1, fb=0, fc=1):
+    f.seek(0)
+    wholemeshread = f.read()
+    wholemeshfind = wholemeshread.find(b"\x03\x01\x00\x01\x03\x01")
+    if wholemeshread != 0:
+        f.seek(wholemeshfind, 0)
+        f.seek(6, 1)
+        vertexcount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(vertexcount):
+            vx = unpack("<f", f.read(4))[0]
+            vy = unpack("<f", f.read(4))[0]
+            vz = unpack("<f", f.read(4))[0]
+            nz = unpack("<f", f.read(4))[0]
+            vertices.append([x,vy,vz])
+        for i in range(vertexcount-2):
+            fa +=1
+            fb +=1
+            fc +=1
+            vertices.append([fa,fb,fc])
+        f.seek(6, 1)
+        uvcount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(uvcount):
+            f.seek(4, 1)
+        f.seek(6, 1)
+        colorcount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(colorcount):
+            f.seek(1,1)
+            f.seek(1,1)
+            f.seek(1,1)
+            f.seek(1,1)
+        mesh = bpy.data.meshes.new("dragonjan")
+        mesh.from_pydata(vertices, [], faces)
+        object = bpy.data.objects.new("dragonjan", mesh)
+        bpy.context.collection.objects.link(object)
+        mesh.vertex_colors.new(name="dragonjanCol")
+        bpy.data.materials.new(name="default")
+        uvtex = mesh.uv_layers.new()
+        uvtex.name = 'dragonjanUV'
+        for fac in mesh.polygons:
+            fac.use_smooth = True
+    else:
+        raise Exception("that offset not found")"""
+        
+            
+        
+        
+            
+        
+
+def selectUVThird(f, uvs=[]):
+    f.seek(0)
+    uvthirdmatchread = f.read()
+    uvthifind = uvthirdmatchread.find(b"\x03\x01\x00\x01\x03\x80")
+    #match this offset to get the uv target since it's not possible with this offset
+    if uvthirdmatchread != 0:
+        f.seek(uvthifind, 0)
+        f.seek(6, 1)
+        vertexcount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(vertexcount):
+            f.seek(4, 1)
+            f.seek(4, 1)
+            f.seek(4, 1)
+            f.seek(4, 1)
+        f.seek(2, 1)
+        uvcount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(uvcount):
+            ux = unpack("<h", f.read(2))[0] / 10000.0
+            uy = unpack("<h", f.read(2))[0] / 10000.0
+            uvs.append([ux,uy])
+        obdata = bpy.context.object.data
+        uv_tex = obdata.uv_layers.new()
+        uv_layer = obdata.uv_layers[0].data
+        vert_loops = {}
+        for l in obdata.loops:
+            vert_loops.setdefault(l.vertex_index, []).append(l.index)
+        for i, coord in enumerate(uvs):
+            for li in vert_loops[i]:
+                uv_layer[li].uv = coord
+    #whale chase mouth 0x0480XX65
+
+def select2ndUV(f, uvs=[]):
+
+    f.seek(0)
+    uvsecondread = f.read()
+    uvsecondfind = uvsecondread.find(b"\x01\x00\x00\x05\x04\x80")
+    if uvsecondread != 0:
+        f.seek(uvsecondfind, 0)
+        f.seek(6, 1)
+        uvcount = unpack("b", f.read(1))[0]
+        f.seek(1,1) #0x65 in a row
+        for i in range(uvcount):
+            ux = unpack("<h", f.read(2))[0] / 10000.0
+            uy = unpack("<h", f.read(2))[0] / 10000.0
+            uvs.append([ux, uy])
+        obdata = bpy.context.object.data
+        uv_tex = obdata.uv_layers.new()
+        uv_layer = obdata.uv_layers[0].data
+        vert_loops = {}
+        for l in obdata.loops:
+            vert_loops.setdefault(l.vertex_index, []).append(l.index)
+        for i, coord in enumerate(uvs):
+            for li in vert_loops[i]:
+                uv_layer[li].uv = coord
+        
+
+def selectUV(f, uvs=[]):
+
+    f.seek(0)
+    uvfirstread = f.read()
+    uvfirstfind = uvfirstread.find(b"\x01\x00\x00\x05\x04\x80")
+    if uvfirstread != 0:
+        f.seek(uvfirstfind, 0)
+        f.seek(6, 1)
+        uvcount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(uvcount):
+            ux = unpack("<h", f.read(2))[0] / 10000.0
+            uy = unpack("<h", f.read(2))[0] / 10000.0
+            f.seek(4, 1)
+            uvs.append([ux, uy])
+        obdata = bpy.context.object.data
+        uv_tex = obdata.uv_layers.new()
+        uv_layer = obdata.uv_layers[0].data
+        vert_loops = {}
+        for l in obdata.loops:
+            vert_loops.setdefault(l.vertex_index, []).append(l.index)
+        for i, coord in enumerate(uvs):
+            for li in vert_loops[i]:
+                uv_layer[li].uv = coord
+            
+            
+def get_2ndobjects(f, vertices=[], chunks2=0, paddingWhere2=0, chunk2=0):
+    f.seek(0)
+    twoobjectsread = f.read()
+    twoobjectfound = twoobjectsread.find(b"\x00\x00\x00\x00\x02\x80")
+    if twoobjectsread != 0:
+        f.seek(twoobjectfound,0)
+        f.seek(chunk2, 1)
+        f.seek(6, 1)
+        vertexcount = unpack("b", f.read(1))[0]
+        f.seek(1, 1)
+        for i in range(vertexcount):
+            f.seek(4,1)
+            vx = unpack("<f", f.read(4))[0]
+            vy = unpack("<f", f.read(4))[0]
+            vz = unpack("<f", f.read(4))[0]
+            nz = unpack("<f", f.read(4))[0]
+            vertices.append([vx,vy,vz])
+        mesh = bpy.data.meshes.new("dragonjan")
+        mesh.from_pydata(vertices, [], [])
+        object = bpy.data.objects.new("dragonjan", mesh)
+        bpy.context.collection.objects.link(object)
+
+def get_1stobjectspt2(f, fa=-1,fb=0,fc=1,vertices=[], faces=[]):
+    f.seek(0)
+    objectsread = f.read()
+    objectfound = objectsread.find(b"\x03\x01\x00\x01\x03\x80")
+    if objectsread != 0:
+        f.seek(objectfound, 0)
+        f.seek(chunk,1)
+        f.seek(6, 1)
+        vertexcount = unpack("b", f.read(1))[0]
+        f.seek(1, 1)
+        for i in range(vertexcount):
+            vx = unpack("<f", f.read(4))[0]
+            vy = unpack("<f", f.read(4))[0]
+            vz = unpack("<f", f.read(4))[0]
+            nz = unpack("<f", f.read(4))[0]
+            vertices.append([vx, vy, vz])
+        for i in range(vertexcount-2):
+            fa+=1
+            fb+=1
+            fc+=1
+            faces.append([fa,fb,fc])
+            
+                
+        mesh = bpy.data.meshes.new("dragonjan")
+        mesh.from_pydata(vertices, [], faces)
+        object = bpy.data.objects.new("dragonjan", mesh)
+        bpy.context.collection.objects.link(object)
+        mesh.vertex_colors.new(name="dragonjanCol")
+        bpy.data.materials.new(name="default")
+        uvtex = mesh.uv_layers.new()
+        uvtex.name = 'dragonjanUV'
+        for fac in mesh.polygons:
+            fac.use_smooth = True
+    else:
+        raise Exception("that offset not found")
+    
+
+def get_1stobjects(f, fa=-1,fb=0,fc=1,vertices=[], faces=[], uvs=[], rgba=[], chunks=0, paddingWhere=0):
+    f.seek(0)
+    firstOBJread = f.read()
+    readobjfirst = firstOBJread.find(b"\x03\x01\x00\x01\x03\x80")
+    if firstOBJread != 0:
+        f.seek(readobjfirst,0)
+        f.seek(6,1)
+        vertexCount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(vertexCount):
+            vx = unpack("<f", f.read(4))[0]
+            vy = unpack("<f", f.read(4))[0]
+            vz = unpack("<f", f.read(4))[0]
+            nz = unpack("<f", f.read(4))[0]
+            vertices.append([vx,vy,vz])
+        for i in range(vertexCount-2):
+            fa += 1
+            fb += 1
+            fc += 1
+            faces.append([fa,fb,fc])
+        f.seek(6, 1)
+        uvcount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(uvcount):
+            f.seek(4,1)
+            ux = unpack("h", f.read(2))[0] / 10000.0
+            uy = unpack("h", f.read(2))[0] / 10000.0
+            uvs.append([ux,uy])
+        f.seek(6, 1)
+        rgbacount = unpack("b", f.read(1))[0]
+        f.seek(1,1)
+        for i in range(rgbacount):
+            r = unpack("b", f.read(1))[0] / 255.0
+            g = unpack("b", f.read(1))[0] / 255.0
+            b = unpack("b", f.read(1))[0] / 255.0
+            a = unpack("b", f.read(1))[0] / 127.0
+            rgba.append([r,g,b])
+            
+                
+        mesh = bpy.data.meshes.new("dragonjan")
+        mesh.from_pydata(vertices, [], faces)
+        object = bpy.data.objects.new("dragonjan", mesh)
+        bpy.context.collection.objects.link(object)
+        mesh.vertex_colors.new(name="dragonjanCol")
+        bpy.data.materials.new(name="default")
+            
+        for fac in mesh.polygons:
+            fac.use_smooth = True
+        
+        
+
+
+def get_INST(f,emptytime=[]):
+    f.seek(0)
+    INSTread = f.read()
+    INSTfound = INSTread.find(b"\x49\x4E\x53\x54")
+    if INSTread != 0:
+        f.seek(INSTfound, 0)
+        f.seek(8,1)
+        INSTS = unpack("<i", f.read(4))[0]
+        INSTsize = unpack("<i", f.read(4))[0]
+        INSTcount = unpack("<i", f.read(4))[0]
+        unk01 = unpack("<i", f.read(4))[0]
+        for i in range(INSTcount):
+            INSTScaleX = unpack("<f", f.read(4))[0]
+            f.seek(16,1)
+            INSTScaleY = unpack("<f", f.read(4))[0]
+            f.seek(16,1)
+            INSTScaleZ = unpack("<f", f.read(4))[0]
+            f.seek(4,1)
+            INSTxTrans = unpack("<f", f.read(4))[0]
+            INSTyTrans = unpack("<f", f.read(4))[0]
+            INSTzTrans = unpack("<f", f.read(4))[0]
+            INSTUNKFloat = unpack("<f", f.read(4))[0]
+            f.seek(16, 1)
+            bpy.ops.object.empty_add(type='CUBE', align='WORLD', location=([INSTxTrans, INSTyTrans,INSTzTrans]), scale=([INSTScaleX, INSTScaleY, INSTScaleZ]))
+    else:
+        raise Exception("no INST found")
+
+def get_SPEC(f, emptytime2=[]):
+    f.seek(0)
+    SPECread = f.read()
+    SPECfound = SPECread.find(b"\x53\x50\x45\x43")
+    if SPECread != 0:
+        f.seek(SPECfound,0)
+        SPECS = unpack("<i", f.read(4))[0]
+        SPECSsize = unpack("<i", f.read(4))[0]
+        SPECcount = unpack("<i", f.read(4))[0]
+        unk01 = unpack("<i", f.read(4))[0]
+        for i in range(SPECcount):
+            SPECScaleX = unpack("<f", f.read(4))[0]
+            f.seek(16,1)
+            SPECScaleY = unpack("<f", f.read(4))[0]
+            f.seek(16,1)
+            SPECScaleZ = unpack("<f", f.read(4))[0]
+            f.seek(4,1)
+            SPECxTrans = unpack("<f", f.read(4))[0]
+            SPECyTrans = unpack("<f", f.read(4))[0]
+            SPECzTrans = unpack("<f", f.read(4))[0]
+            SPECUNKFloat = unpack("<f", f.read(4))[0]
+            f.seek(16, 1)
+            bpy.ops.object.empty_add(type='CUBE', align='WORLD', location=([SPECxTrans, SPECyTrans,SPECzTrans]), scale=([SPECScaleX, SPECScaleY, SPECScaleZ]))
+
+def get_SPEC_ANIMATION_BLOCK(f, emptytime3=[]):
+    f.seek(0)
+    IABLread = f.read()
+    IABLsearch = IABLread.find(b"\x49\x41\x42\x4C")
+    if IABLread != 0:
+        f.seek(IABLsearch,0)
+        IABLS_ram = unpack("<i", f.read(4))[0]
+        IABLS_size = unpack("<i", f.read(4))[0]
+        IABLcount = unpack("<i", f.read(4))[0]
+        unk01 = unpack("<i", f.read(4))[0]
+        for i in range(IABLcount):
+            IABLScaleX = unpack("<f", f.read(4))[0]
+            f.seek(16,1)
+            IABLScaleY = unpack("<f", f.read(4))[0]
+            f.seek(16,1)
+            IABLScaleZ = unpack("<f", f.read(4))[0]
+            f.seek(4,1)
+            IABLxTrans = unpack("<f", f.read(4))[0]
+            IABLyTrans = unpack("<f", f.read(4))[0]
+            IABLzTrans = unpack("<f", f.read(4))[0]
+            IABLUNKFloat = unpack("<f", f.read(4))[0]
+            f.seek(32, 1)
+            bpy.ops.object.empty_add(type='CUBE', align='WORLD', location=([IABLxTrans, IABLyTrans,IABLzTrans]), scale=([IABLScaleX, IABLScaleY, IABLScaleZ]))
+
+def get_Animation_LIBRARY(f):
+    f.seek(0)
+    ALIBread = f.read()
+    ALIBsearch = ALIBread.find(b"\x41\x4C\x49\x42")
+    if ALIBread != 0:
+        f.seek(ALIBsearch,0)
+        ALIBS = unpack("<i", f.read(4))[0]
+        ALIBSize = unpack("<i", f.read(4))[0]
+        ALIBcount = unpack("<i", f.read(4))[0]
+        unk01 = unpack("<i", f.read(4))[0]
+        ALIBframecount = unpack("<i", f.read(4))[0]
+        f.seek(4,1)
+        f.seek(4,1)
+        f.seek(20, 1)
+        keyframecount = unpack("<i", f.read(4))[0]
+        bpy.context.scene.frame_current=1
+        bpy.context.scene.frame_start=1
+        bpy.context.scene.frame_end=ALIBframecount
+        #keyframes not available yet due lack of incompleted random bytes with chunks
+        
+
+def get_splineset(f, ea=-1, eb=0, vertices=[], edges=[]):
+    f.seek(0)
+    splinesetread = f.read()
+    splinesearch = splinesetread.find(b"\x53\x53\x54\x30")
+    if splinesetread != 0:
+        f.seek(splinesearch, 0)
+        SST0S = unpack("i", f.read(4))[0]
+        SST0Size = unpack("i",f.read(4))[0]
+        SST0E1 = unpack("i",f.read(4))[0]
+        SplineTotalSize = unpack("i",f.read(4))[0]
+        SST0E2 = unpack("i",f.read(4))[0]
+        f.seek(4, 1)
+        for i in range(SST0E2):
+            vx = unpack("<f",f.read(4))[0]
+            vy = unpack("<f",f.read(4))[0]
+            vz = unpack("<f",f.read(4))[0]
+            vertices.append([vx, vy, vz])
+        for e in range(SST0E2-1):
+            ea += 1
+            eb += 1
+            edges.append([ea,eb])
+        mesh = bpy.data.meshes.new("SplineSet")
+        mesh.from_pydata(vertices, edges, [])
+        object = bpy.data.objects.new("SplineSet", mesh)
+        bpy.context.collection.objects.link(object)
+    else:
+        raise Exception("no spline found")
+
+def parse_gsc(filepath, WholeOne=False, ONE_CHUNK_OFFSET1=False, ONE_CHUNK_OFFSET1PT2=False, ONE_CHUNK_OFFSET2=False, SST=False, SPEC=False, INST=False, IABL=False, SelectOnlyUVMesh=False, SelectOnly2ndUVMesh=False, SelectOnly3rdUVMesh=False):
+    f = open(filepath, "rb")
+    if WholeOne:
+        wholeChunk1(f, vertices=[])
+    if ONE_CHUNK_OFFSET1:
+        get_1stobjects(f, fa=-1,fb=0,fc=1,vertices=[], faces=[], uvs=[], rgba=[], chunks=0, paddingWhere=0)
+    if ONE_CHUNK_OFFSET1PT2:
+        get_1stobjectspt2(f, fa=-1,fb=0,fc=1,vertices=[], faces=[])
+    if ONE_CHUNK_OFFSET2:
+        get_2ndobjects(f, vertices=[], chunks2=0, paddingWhere2=0, chunk2=0)
+    if INST:
+        get_INST(f,emptytime=[])
+    if SPEC:
+        get_SPEC(f, emptytime2=[])
+    if IABL:
+        get_SPEC_ANIMATION_BLOCK(f, emptytime3=[])
+    if SST:
+        get_splineset(f, ea=-1, eb=0, vertices=[], edges=[])
+    if SelectOnlyUVMesh:
+        selectUV(f, uvs=[])
+    if SelectOnly2ndUVMesh:
+        select2ndUV(f, uvs=[])
+    if SelectOnly3rdUVMesh:
+        selectUVThird(f, uvs=[])
+    f.close()
+
+
+
