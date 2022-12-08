a6b5193ce2fb2eb1c5d7cfbb7a7bc1923cab813a
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2022-12-08 10:32:05 -0800

    Update __init__.py
    ...

--- io_chunkscene_gsc/__init__.py
+++ io_chunkscene_gsc/__init__.py
@@ -1,17 +1,18 @@
 bl_info = {
         'name'			: 'Finding Nemo GSC Level Chunk Importer',
 	'author'		: 'DarkShadow Nemo',
-	'version'		: (0, 1, 5),
+	'version'		: (0, 1, 8),
 	'blender'		: (3, 0, 0),
 	'location'		: 'File > Import',
 	'description'           : 'Import GSC one mesh chunk makes it easier',
 	'category'		: 'Chunk-Importer',
+        'warning'               : 'Exporter is Non-Functioned and missing ingame it might work for customs'
 }
 import os
 import bpy
 import importlib
 from bpy.props import CollectionProperty, StringProperty, BoolProperty, EnumProperty, FloatProperty, IntProperty
-from bpy_extras.io_utils import ImportHelper
+from bpy_extras.io_utils import ImportHelper, ExportHelper
 
 from.import gsc_chunk_importer
 
@@ -28,6 +29,10 @@ class ImportChunkGSC(bpy.types.Operator, ImportHelper):
         WholeOne: BoolProperty(
                 name        = '0x03010001 all offsets',
                 description = 'if they match it imports all the whole chunk in'
+        )
+        WholeMassiveOne: BoolProperty(
+                name        = '0x00000280 all offsets',
+                description = 'unfortanately this imports vertices only no idea to import faces for this offset'
         )
         ONE_CHUNK_OFFSET1: BoolProperty(
                 name        = '0x030100010380',
@@ -53,6 +58,10 @@ class ImportChunkGSC(bpy.types.Operator, ImportHelper):
                 name        = 'InstanceAnimationBlock',
                 description = 'imports the stuff that you replaced in ram'
         )
+        BoundingSet: BoolProperty(
+                name        = 'BNDS',
+                description = 'imports boundingsets'
+        )
         ONE_CHUNK_OFFSET1PT2: BoolProperty(
                 name = "0x030100010380 Part 2",
                 description = 'supports now dodgy lego star wars and nemo there nemo offset starts at 0x0480XX6D and uv and rgba data for lego star wars'
@@ -75,8 +84,9 @@ class ImportChunkGSC(bpy.types.Operator, ImportHelper):
                 paths = [os.path.join(self.directory, name.name) for name in self.files]
                 if not paths: paths.append(self.filepath)
                 importlib.reload(gsc_chunk_importer)
-                for path in paths: gsc_chunk_importer.parse_gsc(path, WholeOne = self.WholeOne, ONE_CHUNK_OFFSET1 = self.ONE_CHUNK_OFFSET1,ONE_CHUNK_OFFSET1PT2 = self.ONE_CHUNK_OFFSET1PT2,ONE_CHUNK_OFFSET2 = self.ONE_CHUNK_OFFSET2,SST = self.SST,SPEC = self.SPEC,INST = self.INST,IABL = self.IABL, SelectOnlyUVMesh = self.SelectOnlyUVMesh, SelectOnly2ndUVMesh = self.SelectOnly2ndUVMesh, SelectOnly3rdUVMesh = self.SelectOnly3rdUVMesh)
+                for path in paths: gsc_chunk_importer.parse_gsc(path, WholeOne = self.WholeOne, WholeMassiveOne = self.WholeMassiveOne, ONE_CHUNK_OFFSET1 = self.ONE_CHUNK_OFFSET1,ONE_CHUNK_OFFSET1PT2 = self.ONE_CHUNK_OFFSET1PT2,ONE_CHUNK_OFFSET2 = self.ONE_CHUNK_OFFSET2,SST = self.SST,SPEC = self.SPEC,INST = self.INST,IABL = self.IABL,BoundingSet = self.BoundingSet, SelectOnlyUVMesh = self.SelectOnlyUVMesh, SelectOnly2ndUVMesh = self.SelectOnly2ndUVMesh, SelectOnly3rdUVMesh = self.SelectOnly3rdUVMesh)
                 return {'FINISHED'}
+	
 def menu_func_import(self, context):
         self.layout.operator(ImportChunkGSC.bl_idname, text='GSC Chunk Importer (.gsc)')
 def register():
