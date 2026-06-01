30790c6a28ce2ff1b4c88dae1fd4fdfeea820f9c
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2026-06-01 07:00:05 -0700

    replace flags to 108 to 104

--- io_chunkscene_gsc/exportGSCLib/strips/autoStrips/autostrips_0x03010010380XX68.py
+++ io_chunkscene_gsc/exportGSCLib/strips/autoStrips/autostrips_0x03010010380XX68.py
@@ -386,7 +386,7 @@ def vertices_0x03010010380XX68_(f):
             f.write(pack("B", 3))
             f.write(pack("B", 128))
             f.write(pack("B", len(obdata.vertices)))
-            f.write(pack("B", 108))
+            f.write(pack("B", 104))
             indexFaces = -1
             for v in obdata.vertices:
                 f.write(pack("<f", v.co.x))
