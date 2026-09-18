5452d90247e5f20e6a678fae3ecbd01f0aeeb0dc
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2026-09-18 18:04:40 +0100

    change old to new default strips, todo get 4th next

--- io_chunkscene_gsc/importGSCLib/strips/cleanup/chunk_default_strips.py
+++ io_chunkscene_gsc/importGSCLib/strips/cleanup/chunk_default_strips.py
@@ -7,7 +7,7 @@ from io import BytesIO as bio
 def wholeChunk1_default(f):
     fa_def=-3
     fb_def=-2
-    fc_def-1
+    fc_def=-1
 
     verts_def=[]
     faces_def=[]
@@ -24,11 +24,11 @@ def wholeChunk1_default(f):
             if flagsssss3 == 0x6C:
                 if vertexCount3 == 0:
                     pass
-                if vertexCount3 == 1:
+                elif vertexCount3 == 1:
                     pass
-                if vertexCount3 == 2:
+                elif vertexCount3 == 2:
                     pass
-                if vertexCount3 == 3:
+                elif vertexCount3 == 3:
                     
                     for j in range(1):
                         default_vx = unpack("<f", f.read(4))[0]
@@ -49,33 +49,52 @@ def wholeChunk1_default(f):
                         default_type42 = unpack("B", f.read(1))[0]
                         default_value12 = unpack("B", f.read(1))[0]
                         default_nz2 = unpack("<h", f.read(2))[0]
-                        if default_value1 == 128 and default_value11 == 128 and default_value == 0:
-                            if default_type4 == 1 and default_type41 == 1 and default_type42 == 0:
-                                #inverted strips and tri
-                                
-                                verts_def.append([default_vx,default_vz,default_vx])
-                                verts_def.append([default_vx1,default_vz1,default_vy1])
-                                verts_def.append([default_vx2,default_vz2mdefault_vy2])
+                    _00_n_offset3_default = unpack("<I", f.read(4))[0]
+                    if _00_n_offset3_default == 16777473:
+                        _00_n_offset4_default = unpack("<I", f.read(4))[0]
+                        if _00_n_offset4_default == 335545088:
+                            if default_type4 == 1:
+                                if default_type41 == 1:
+                                    if default_type42 == 0:
+                                        verts_def.append([default_vx,default_vz,default_vx])
+                                        verts_def.append([default_vx1,default_vz1,default_vy1])
+                                        verts_def.append([default_vx2,default_vz2,default_vy2])
 
-                                fa_def+=1*3
-                                fb_def+=1*3
-                                fc_def+=1*3
-
-                                faces_def.append([fb_def,fa_def,fc_def])
-
-                        elif default_value1 == 0 and default_value11 == 0 and default_value == 0:
-                            if default_type4 == 1 and default_type41 == 1 and default_type42 == 0:
-                                #strips and tri
-                                
-                                verts_def.append([default_vx,default_vz,default_vx])
-                                verts_def.append([default_vx1,default_vz1,default_vy1])
-                                verts_def.append([default_vx2,default_vz2,default_vy2])
-
-                                fa_def+=1*3
-                                fb_def+=1*3
-                                fc_def+=1*3
-
-                                faces_def.append([fa_def,fb_def,fc_def])
+                                        fa_def+=1*3
+                                        fb_def+=1*3
+                                        fc_def+=1*3
+                                        faces_def.append([fa_def,fb_def,fc_def])
+                elif vertexCount3 == 4:
+                    for i in range(1):
+                        default_vx_ = unpack("<f", f.read(4))[0]
+                        default_vy_ = unpack("<f", f.read(4))[0]
+                        default_vz_ = unpack("<f", f.read(4))[0]
+                        default_type4_ = unpack("B", f.read(1))[0]
+                        default_value1_ = unpack("B", f.read(1))[0]
+                        default_nz_ = unpack("<h", f.read(2))[0]
+                        default_vx1_ = unpack("<f", f.read(4))[0]
+                        default_vy1_ = unpack("<f", f.read(4))[0]
+                        default_vz1_ = unpack("<f", f.read(4))[0]
+                        default_type41_ = unpack("B", f.read(1))[0]
+                        default_value11_ = unpack("B", f.read(1))[0]
+                        default_nz1_ = unpack("<h", f.read(2))[0]
+                        default_vx2_ = unpack("<f", f.read(4))[0]
+                        default_vy2_ = unpack("<f", f.read(4))[0]
+                        default_vz2_ = unpack("<f", f.read(4))[0]
+                        default_type42_ = unpack("B", f.read(1))[0]
+                        default_value12_ = unpack("B", f.read(1))[0]
+                        default_nz3_ = unpack("<h", f.read(2))[0]
+                        default_vx3_ = unpack("<f", f.read(4))[0]
+                        default_vy3_ = unpack("<f", f.read(4))[0]
+                        default_vz3_ = unpack("<f", f.read(4))[0]
+                        default_type43_ = unpack("B", f.read(1))[0]
+                        default_value13_ = unpack("B", f.read(1))[0]
+                        default_nz3_ = unpack("<h", f.read(2))[0]
+                    _00_n_offset3_default1 = unpack("<I", f.read(4))[0]
+                    if _00_n_offset3_default1 == 16777473:
+                        _00_n_offset4_default1 = unpack("<I", f.read(4))[0]
+                        if _00_n_offset4_default1 == 335545088:
+                            pass
 
                                 
                             
